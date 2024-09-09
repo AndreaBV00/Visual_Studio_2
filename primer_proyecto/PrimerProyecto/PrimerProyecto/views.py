b95255faada4f75a93b179918d266ce7bf8c010c
AndreaBV00 <azulaitami@gmail.com>
2024-09-09 19:55:48 -0300

    fin_clase_1_django

--- primer_proyecto/PrimerProyecto/PrimerProyecto/views.py
+++ primer_proyecto/PrimerProyecto/PrimerProyecto/views.py
@@ -8,7 +8,7 @@ def probando_templates(request):
     mi_html = open("C:\Users\ANDREABRITO\OneDrive - Partry S.A\1Andrea\Visual_Studio_2\primer_proyecto\PrimerProyecto\PrimerProyecto\templates\template1.html")
     platilla = Template(mi_html.read())
     mi_html.close()
-    mi_contexto = Context()
+    mi_contexto = Context() # Contexto vacío, el contexto es un diccionario, sirve para pasar variables a la plantilla
     documento = platilla.render(mi_contexto)
 
     return HttpResponse(documento)
