029dc4f5e5c4eabf1c59546a1b237393e9b181ee
AndreaBV00 <azulaitami@gmail.com>
2024-09-09 19:45:07 -0300

    template

--- primer_proyecto/PrimerProyecto/PrimerProyecto/urls.py
+++ primer_proyecto/PrimerProyecto/PrimerProyecto/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import saludo
+from .views import saludo, probando_templates
 
 urlpatterns = [
    path('ad min/', admin.site.urls),
    path('saluda/', saludo),
+   path('probando_templates/', probando_templates),
 ]
--- primer_proyecto/PrimerProyecto/PrimerProyecto/views.py
+++ primer_proyecto/PrimerProyecto/PrimerProyecto/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 
 def saludo(request):
     return HttpResponse("Hola, mundo. Esta es mi primera página web.")
+
+def probando_templates(request):
+    mi_html = open("C:\Users\ANDREABRITO\OneDrive - Partry S.A\1Andrea\Visual_Studio_2\primer_proyecto\PrimerProyecto\PrimerProyecto\templates\template1.html")
+    platilla = Template(mi_html.read())
+    mi_html.close()
+    mi_contexto = Context()
+    documento = platilla.render(mi_contexto)
+
+    return HttpResponse(documento)
